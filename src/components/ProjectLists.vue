ae52c060a04dedec6f082154529d543bbd0402b8
bineetNaidu <bineet.s.naidu@gmail.com>
2021-01-27 23:13:54 +0530

    🎉 add types and axios data refs

---
+++ src/components/ProjectLists.vue
@@ -0,0 +1,19 @@
+<template>
+  div
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+// import { ProjectType } from "@/types";
+
+export default defineComponent({
+  name: 'ProjectList',
+  // props: {
+  //    projects: Array<ProjectType>,
+  // }
+});
+</script>
+
+<style lang="sass" scoped>
+
+</style>
