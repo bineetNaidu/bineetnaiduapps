9942b2027cbee224cb2c49fd30e8c76f9fdb4aed
bineetNaidu <bineet.s.naidu@gmail.com>
2021-01-28 22:19:07 +0530

    🚀 add tailwindcss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="app">
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,19 @@
-import Head from 'next/head';
-
 export default function Home() {
   return (
-    <div className="welcome__board">
-      <h1>
-        Hello 👋👋, I'm
-        <a
-          className="link"
-          href="https://bineetnaidu-io.web.app"
-          target="_black"
-          rel="noopener"
-        >
-          Bineet
-        </a>
-      </h1>
+    <div className="bg-green-100">
+      <div className="flex justify-center text-4xl py-4">
+        <h1>
+          Hello 👋👋, I'm
+          <a
+            className="link"
+            href="https://bineetnaidu-io.web.app"
+            target="_black"
+            rel="noopener"
+          >
+            Bineet
+          </a>
+        </h1>
+      </div>
     </div>
   );
 }
