a846244080b8c65aa3df5feb4ffbfbafb8e6a7f8
bineetNaidu <bineet.s.naidu@gmail.com>
2021-01-29 15:13:11 +0530

    🎉 add Featured Page

---
+++ pages/featured.tsx
@@ -0,0 +1,32 @@
+import { GetStaticProps, InferGetStaticPropsType } from 'next';
+import axios from '../utils/axios';
+import ProjectList from '../components/ProjectList';
+import { ProjectType } from '../utils/types';
+
+export default function Featured({
+  projects,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  return (
+    <div className="bg-gray-900 text-white app font-mono">
+      <ProjectList projects={projects} />
+    </div>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const { data } = await axios.get('/?featured=true');
+  if (!data.success && !data.errors) {
+    return {
+      props: { projects: [] },
+    };
+  }
+
+  const projects: ProjectType[] = data.data;
+
+  return {
+    props: {
+      projects,
+    },
+    revalidate: 1, // ? In Seconds
+  };
+};
